673ff3d619cf1e491819f997edf12953a89adac6
SmokeyLowkey <yuriykondakov8@gmail.com>
2024-05-16 09:51:35 -0600

    modified:   src/layouts/components/SkeletonLoader.tsx

--- src/layouts/components/SkeletonLoader.tsx
+++ src/layouts/components/SkeletonLoader.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const Skeleton = ({ className }) => (
+interface SkeletonProps {
+  className?: string;  // '?' makes the className prop optional
+}
+
+const Skeleton = ({ className = ''}) => (
     <div className={`animate-pulse ${className}`}>
       <div className="bg-gray-300 h-8 rounded"></div>
     </div>
